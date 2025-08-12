fbd4cd5664b55aa8f8acce42e9662ec21167f9db
Prabhakar33362 <calyyxx33@gmail.com>
2025-08-12 08:50:45 +0545

    Add files via upload

---
+++ New Project/custom.js
@@ -0,0 +1,40 @@
+$('.main-car').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.test').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items: 5
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
